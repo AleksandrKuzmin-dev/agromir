54e8877a882e394fd3a25c9b408f0281d8ae1813
Aleksandr Kuzmin <a-kuzmin.dev@yandex.ru>
2024-02-03 12:23:59 +0400

    Реализовал функционал открытия модальных окон и меню, добавил меню на все страницы

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,53 @@
+'use strict'
+document.addEventListener('DOMContentLoaded', () => {
 
+   /* Открытие/закрытие модальных окон + меню */
+   function modal(triggerSelector, modalSelector, closeSelector) {
+        const modal = document.querySelector(modalSelector),
+              trigger = document.querySelectorAll(triggerSelector),
+              close = document.querySelector(closeSelector),
+              body = document.querySelector('body');
+
+        function openModal(){
+            modal.classList.remove('none');
+            body.style.overflow = 'hidden';
+
+            if(modalSelector === '.menu') {
+                setTimeout(() => modal.style.height = '100%', 40);
+            }
+        };
+
+        function closeModal(){
+            if(modalSelector === '.menu') {
+                modal.style.height = '0%';
+                setTimeout(() => modal.classList.add('none'), 600);
+            } else {
+                modal.classList.add('none');
+            }
+
+            body.style.overflow = 'unset';
+        }
+
+        trigger.forEach((item) => {
+            item.addEventListener('click', () => {
+                openModal();
+            });
+        });
+
+        close.addEventListener('click', () => {
+            closeModal();
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) closeModal();
+        });
+
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape') closeModal();
+        });
+   };
+
+   modal('.header__burger-icon', '.menu', '.menu__close');
+
+
+});
