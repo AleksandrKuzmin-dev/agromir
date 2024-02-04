0744e3fdf21aeef85b8dfdb3db825bbb6a0a937d
Aleksandr Kuzmin <a-kuzmin.dev@yandex.ru>
2024-02-04 17:01:01 +0400

    Реализовал функционал открытия видео в модальных окнах

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', () => {
 
-   /* Открытие/закрытие модальных окон + меню */
-   function modal(triggerSelector, modalSelector, closeSelector) {
+   /* Открытие/закрытие модальных окон (в тюом числе меню и видео)*/
+   function modal(triggerSelector, modalSelector, closeSelector, srcVideo = null) {
         const modal = document.querySelector(modalSelector),
               trigger = document.querySelectorAll(triggerSelector),
               close = document.querySelector(closeSelector),
               body = document.querySelector('body');
 
         function openModal(){
+            if(srcVideo) {
+                modal.children[0].src = srcVideo;
+            };
+
             modal.classList.remove('none');
             body.style.overflow = 'hidden';
 
-            if(modalSelector === '.menu') {
+            if(modalSelector === '.menu' || srcVideo) {
                 setTimeout(() => modal.style.height = '100%', 40);
             }
         };
 
         function closeModal(){
-            if(modalSelector === '.menu') {
+            if(modalSelector === '.menu' || srcVideo) {
                 modal.style.height = '0%';
                 setTimeout(() => modal.classList.add('none'), 600);
             } else {
@@ -48,6 +52,6 @@ document.addEventListener('DOMContentLoaded', () => {
    };
 
    modal('.header__burger-icon', '.menu', '.menu__close');
-
-
+   modal('[data-video="1"]', '.modal-video', '.modal-video__close', './video/main__bg.mp4');
+   modal('[data-video="2"]', '.modal-video', '.modal-video__close', './video/main__bg.mp4');
 });
